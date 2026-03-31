e634fe037a3bbe321397d375bd1068bf3f986846
Reid Nicholson <reid@datastrada.net>
2026-03-31 12:09:32 -0600

    Cleaned code

--- app/src/main/java/net/datastrada/fueleconomyconverter/MainActivity.kt
+++ app/src/main/java/net/datastrada/fueleconomyconverter/MainActivity.kt
@@ -47,53 +47,53 @@ class MainActivity : AppCompatActivity() {
             usMpgResult = inputDouble
 
             // to Imperial MPG
-            impMpgResult =  convertUsMpgToImperialMpg(inputDouble)
+            impMpgResult =  inputDouble * 1.2009499241541
 
             // to KPL
-            kplResult = convertUsMpgToKPL(inputDouble)
+            kplResult = inputDouble * 0.425
 
             // to L/100 KM
-            lP100KmResult = convertUsMpgToLP100KM(inputDouble)
+            lP100KmResult = 100/((inputDouble * 1.609) / 3.785)
 
         // From Imperial MPG
         } else if (inputSelection == findViewById(R.id.fromMpgImperial)) {
             //To US MPG
-            usMpgResult = convertImperialMpgToUsMpg(inputDouble)
+            usMpgResult = inputDouble / 1.2009499241541
 
             // to Imperial MPG
             impMpgResult =  inputDouble
 
             // to KPL
-            kplResult = convertImperialMpgToKpl(inputDouble)
+            kplResult = inputDouble * 0.354
 
             // to L/100 KM
-            lP100KmResult = convertImperialMpgToLP100KM(inputDouble)
+            lP100KmResult = 100 /((inputDouble * 1.609) / 4.546)
 
         // From KPL
         } else if (inputSelection == findViewById(R.id.fromKPL)) {
             //To US MPG
-            usMpgResult = convertKplToUsMpg(inputDouble)
+            usMpgResult = inputDouble * 2.352
 
             // to Imperial MPG
-            impMpgResult =  convertKplToImperialMpg(inputDouble)
+            impMpgResult =  inputDouble * 2.825
 
             // to KPL
             kplResult = inputDouble
 
             // to L/100 KM
-            lP100KmResult = convertKplToLP100KM(inputDouble)
+            lP100KmResult = 100 / inputDouble
 
 
         // from L/100 KM
         } else if (inputSelection == findViewById(R.id.fromLP100KM)) {
             //To US MPG
-            usMpgResult = convertLp100KmToUsMpg(inputDouble)
+            usMpgResult = (3.785 * (100 / inputDouble)) / 1.609
 
             // to Imperial MPG
-            impMpgResult =  convertLp100KmToImperialMpg(inputDouble)
+            impMpgResult =  (4.546 * (100 / inputDouble)) / 1.609
 
             // to KPL
-            kplResult = convertLp100KmToKpl(inputDouble)
+            kplResult = 100 / inputDouble
 
             // to L/100 KM
             lP100KmResult = inputDouble
@@ -105,82 +105,5 @@ class MainActivity : AppCompatActivity() {
         kplResultText.text = kplResult.toString()
         lP100KmResultText.text = lP100KmResult.toString()
 
-    }
-
-
-
-    /*******************************************************************************
-    * From US MPG
-    *******************************************************************************/
-    // To L/100 KM
-    private fun convertUsMpgToLP100KM(inputDouble: Double): Double? {
-        var result = 100/((inputDouble * 1.609) / 3.785)
-        return result
-    }
-
-    // to KPL
-    private fun convertUsMpgToKPL(inputDouble: Double): Double? {
-        var result = inputDouble * 0.425
-        return result
-    }
-
-    // to Imperial MPG
-    private fun convertUsMpgToImperialMpg(inputDouble: Double): Double {
-        var result = inputDouble * 1.2009499241541
-        return result
-    }
-
-    /*******************************************************************************
-     * From Imperial MPG
-     *******************************************************************************/
-    private fun convertImperialMpgToKpl(inputDouble: Double): Double? {
-        var result = inputDouble * 0.354
-        return result
-    }
-
-    private fun convertImperialMpgToLP100KM(inputDouble: Double): Double? {
-        var result = 100 /((inputDouble * 1.609) / 4.546)
-        return result
-    }
-
-    private fun convertImperialMpgToUsMpg(inputDouble: Double): Double? {
-        var result = inputDouble / 1.2009499241541
-        return result
-    }
-
-    /*******************************************************************************
-     * From KPL
-     *******************************************************************************/
-    private fun convertKplToImperialMpg(inputDouble: Double): Double? {
-        var result = inputDouble * 2.825
-        return result
-    }
-
-    private fun convertKplToLP100KM(inputDouble: Double): Double? {
-        var result = 100 / inputDouble
-        return result
-    }
-
-    private fun convertKplToUsMpg(inputDouble: Double): Double? {
-        var result = inputDouble * 2.352
-        return result
-    }
-
-    /*******************************************************************************
-     * From L/100 KM
-     *******************************************************************************/
-    private fun convertLp100KmToImperialMpg(inputDouble: Double): Double? {
-        var result = (4.546 * (100 / inputDouble)) / 1.609
-        return result
-    }
-
-    private fun convertLp100KmToKpl(inputDouble: Double): Double? {
-        var result = 100 / inputDouble
-        return result
-    }
-
-    private fun convertLp100KmToUsMpg(inputDouble: Double): Double? {
-        var result = (3.785 * (100 / inputDouble)) / 1.609
-        return result
     }
 }
